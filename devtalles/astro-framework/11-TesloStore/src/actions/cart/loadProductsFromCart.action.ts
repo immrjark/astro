3c665e821cbf17bd6f77b04a2d1554855ae05e4a
Chema <mrjark7@gmail.com>
2024-10-21 10:43:27 +0200

    i've have an error and i do not know how to fix it with cookies and products cart

--- devtalles/astro-framework/11-TesloStore/src/actions/cart/loadProductsFromCart.action.ts
+++ devtalles/astro-framework/11-TesloStore/src/actions/cart/loadProductsFromCart.action.ts
@@ -1,49 +1,55 @@
 import type CartItem from "@/interfaces/cartItem";
 import { defineAction } from "astro:actions";
+import { z } from "astro:content";
 import { db, eq, inArray, Product, ProductImage } from "astro:db";
-
 
 export const loadProductsFromCart = defineAction({
   accept: 'json',
-  handler: async(_, context) => {
-    const {cookies} = context;
+  input: z.object({
+    cookies: z.string(),
+  }),
+  handler: async ( input, context) => {
+    // const {cookies} = input
+    const {cookies} = context
 
-    const cart = JSON.parse(cookies.get('cart')?.value ?? '[]') as CartItem[];
+    // const cart = JSON.parse(cookies) as CartItem[]
+    const cart = JSON.parse(cookies.get('cart')?.value ?? '[]') as CartItem[]
 
-    if(cart.length === 0) return [];
+    if(cart.length === 0) return []
 
-    // Load de los productos
-    const productId = cart.map(item => item.productId)
-
+    //load de los productos
+    const productsId = cart.map(i => i.productId);
     const dbProducts = await db
       .select()
       .from(Product)
       .innerJoin(ProductImage, eq(Product.id, ProductImage.productId))
-      .where(inArray(Product.id, productId))
-
-    console.log(dbProducts);
+      .where( inArray(Product.id, productsId))
     
+    console.log(dbProducts);
+      
+    // return[]
+    return cart.map(i => {
 
-    return cart.map( item => {
-      const dbProduct = dbProducts.find( p => p.Product.id === item.productId )
-
-      if (!dbProduct) {
-        throw new Error(`Product with id ${item.productId} not found`)
+      const dbProduct = dbProducts.find( p => p.Product.id === i.productId )
+      if( !dbProduct) {
+        throw new Error(`Product with id${i.productId} is not found`)
       }
 
-      const {title, price, slug} = dbProduct.Product;
-      const image = dbProduct.ProductImage.image
+      const {title, price, slug} = dbProduct.Product
+      const image = dbProduct.ProductImage.image;
 
       return {
-        productId: item.productId,
+        productId: i.productId,
         title: title,
-        quantity: item.quantity,
+        size: i.size,
+        quantity: i.quantity,
         image: image.startsWith('http')
           ? image
           : `${import.meta.env.PUBLIC_URL}/images/products/${image}`,
         price: price,
-        slug: slug
+        slug: slug,
       }
+
     })
-  }
-})
+  },
+});
