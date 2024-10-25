ab3f4c43a31ecc87a2c891c4472442af349847a1
Chema <mrjark7@gmail.com>
2024-10-25 12:42:23 +0200

    backend images

--- devtalles/astro-framework/11-TesloStoreUpkeep/src/actions/products/updateProduct.actions.ts
+++ devtalles/astro-framework/11-TesloStoreUpkeep/src/actions/products/updateProduct.actions.ts
@@ -4,6 +4,15 @@ import { db, eq, Product } from "astro:db";
 import { getSession } from "auth-astro/server";
 import { v4 as UUID } from 'uuid';
 
+const MAX_FILE_SIZE = 5_000_000 // 5megas
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+  'image/svg+xml',
+]
+
 export const updateProduct = defineAction({
   accept: 'form',
   input: z.object({
@@ -17,6 +26,18 @@ export const updateProduct = defineAction({
     tags: z.string(),
     title: z.string(),
     type: z.string(),
+
+    // cargar imagenes
+    imageFiles: z.array(
+      z.instanceof(File) // esto es así porque es una instancia de archivo
+        .refine(file => file.size <= MAX_FILE_SIZE, 'Max. image size is 5MG')
+        .refine( file => {
+          if( file.size === 0 ) return true; // porque a veces da un error de uqe se envia una imagen vacia sin tener seleccionada ninguna
+          // se puede hacer con el return o como la forma de arriba
+          return ACCEPTED_IMAGE_TYPES.includes(file.type)
+        }, `Only supported image files are valid, ${ACCEPTED_IMAGE_TYPES.join(',')}`)
+    ).optional()
+        // .refine( file => ACCEPTED_IMAGE_TYPES.includes(file.type), `Only supported image files are valid, ${ACCEPTED_IMAGE_TYPES.join(',')}`).optional()),
   }),
   handler: async (form, context) =>{
 
@@ -29,7 +50,7 @@ export const updateProduct = defineAction({
       throw new Error('Unauthorized')
     }
     // console.log(user);
-    const {id = UUID(), ...rest} = form
+    const {id = UUID(), imageFiles, ...rest} = form
     rest.slug = rest.slug.toLocaleLowerCase().replaceAll(' ', '-').trim()
 
     const product = {
@@ -47,6 +68,8 @@ export const updateProduct = defineAction({
     }
     await db.update(Product).set(product).where(eq(Product.id, id))
     
+    // images
+    console.log(imageFiles);
     
     return product
   }
